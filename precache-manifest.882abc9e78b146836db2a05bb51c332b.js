e32629dc72f7670f61ae4935a0bec52a5f5af052
Atmagyan Singh <singhatmagyan@outlook.com>
2022-05-25 16:06:38 +0530

    Updates

---
+++ precache-manifest.882abc9e78b146836db2a05bb51c332b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bdf88f5fbd61bea62248157ecd61a75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb9c2e138d1ecd2cd3b2",
+    "url": "/static/css/main.09c32e25.chunk.css"
+  },
+  {
+    "revision": "2be442273ce570494753",
+    "url": "/static/js/2.6403a644.chunk.js"
+  },
+  {
+    "revision": "4e9e6a426e4934b119d62e02d6fa5a5c",
+    "url": "/static/js/2.6403a644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb9c2e138d1ecd2cd3b2",
+    "url": "/static/js/main.790ed957.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.790ed957.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
